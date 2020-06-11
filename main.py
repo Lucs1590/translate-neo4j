7501a6284560b3dd3c5ed7cf366d0bcf0456390e
Lucas Brito <lucasbsilva29@gmail.com>
2020-06-10 22:03:51 -0300

    feat(main.py): completing base of development
    -

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 from translate import Translator
 import requests
 from py2neo import Graph, Node
+from time import time
+import json
 
 
 def main():
+    t1 = time()
     connection = connect_database()
-    data = get_data()
-    insert_data(connection, data)
+    dataset = get_data()["tags"]
+    insert_data(connection, dataset, "Ingredient", "name")
+    print(time() - t1)
 
 
 def connect_database():
     return Graph(
-        host='localhost',
-        port=7687,
-        password='',
-        database=''
+        host='54.173.133.27',
+        port=33411,
+        password='welds-symbols-saps'
     )
 
 
 def get_data():
-    return requests.get('https://world.openfoodfacts.org/ingredients.json')
+    return json.loads(requests.get('https://world.openfoodfacts.org/ingredients.json').text)
 
 
-def insert_data(data):
-    ...
+def insert_data(connection, dataset, label, attribute):
+    for data in dataset:
+        translated_data = translate_data(data["name"]).lower()
+
+        ingredient = Node(label, name=translated_data)
+        ingredient.__primarylabel__ = label
+        ingredient.__primarykey__ = attribute
+
+        connection.merge(ingredient)
+
+        print(translated_data)
 
 
-def tranlate_data(data):
-    return Translator(to_lang="pt").translator.translate(data)
+def translate_data(data):
+    return Translator(to_lang="pt").translate(data)
 
 
 if __name__ == "__main__":
